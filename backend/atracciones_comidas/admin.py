865235bb44a8f9079d5f68e0719c9c4f181876b2
Trebys <estelopez2014@gmail.com>
2026-05-28 20:21:26 -0600

    SCRUM-36 registrar atracciones y comidas en admin

--- backend/atracciones_comidas/admin.py
+++ backend/atracciones_comidas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Attractions, Food
+
+
+@admin.register(Attractions)
+class AttractionsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'usage_points')
+    search_fields = ('name', 'description')
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name', 'description')
